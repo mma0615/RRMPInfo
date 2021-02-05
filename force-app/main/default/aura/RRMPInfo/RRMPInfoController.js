c394b56c567f13cc6bb5013b9d0ba25e55ead01e
Mikey Ma <mikeyma1960@gmail.com>
2021-02-04 16:54:41 -0800

    Initial commit

--- force-app/main/default/aura/RRMPInfo/RRMPInfoController.js
+++ force-app/main/default/aura/RRMPInfo/RRMPInfoController.js
@@ -1,5 +1,6 @@
 ({
-    init : function(component) {
+    init : function(component, event, helper) {
+        /**** get Contact Info from controller */
         var action = component.get("c.getRRMPContactInfo");
         action.setParams({
             "contactType" : component.get("v.conType")
@@ -8,6 +9,7 @@
             var state = response.getState();
             if (state == "SUCCESS"){
                 var conInfo = response.getReturnValue();
+                /***** if contact info found, set screen component values */
                 if (conInfo != null)
                 {
                     component.set("v.recordId", conInfo.Id);
@@ -16,6 +18,9 @@
                     component.set("v.ContactNumber", conInfo.Contact_Number__c);
                     component.set("v.FaxNumber", conInfo.Fax_Number__c);
                 }
+                else
+                    /***** No Record Found */
+                    component.set("v.scrnMsg", "NoRecordFound");
                 
             } else {
                 var toastEvent = $A.get("e.force:showToast");
